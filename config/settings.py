c986a1a426d3b08d82abef6743309c696ddca7c1
Ekaterina <katyat1@mail.ru>
2025-01-12 21:36:27 +1000

    Все работает

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from django.conf.global_settings import STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,6 +93,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
