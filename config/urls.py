8cae018788d0c408944eba3effd5208d6b5a837a
EkaterinaZaitcev <katyat1@mail.ru>
2025-01-21 14:46:38 +1000

    Настроен URL

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from config import settings
+from catalog.apps import CatalogConfig
+from django.conf.urls.static import static
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog'))
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
