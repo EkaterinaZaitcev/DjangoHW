4dd1549791faa30a59b2a588131df29dc7c892e4
EkaterinaZaitcev <katyat1@mail.ru>
2025-01-22 22:18:33 +1000

    Созданы новый контроллер, шаблон со страницой продуктов

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,11 +3,12 @@ from catalog.apps import CatalogConfig
 
 from catalog.views import home
 from catalog.views import contacts
-
+from catalog.views import product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
+    path('product/', product)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 def home(request):
     return render(request, 'home.html')
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+def product(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'product_list.html', context)
