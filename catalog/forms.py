57b95af9164aa72834f66c5fb5f723c12e8ebfa0
EkaterinaZaitcev <katyat1@mail.ru>
2025-02-14 20:41:58 +1000

    Исправлены еще замечания

--- catalog/forms.py
+++ catalog/forms.py
@@ -21,24 +21,26 @@ class ProductForm(StyleFormMixin, ModelForm):
         ban_words = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"]
         cleaned_data = super().clean()
         name = cleaned_data.get('name')
-        for word in ban_words:
-            if word in name.lower():
-                raise ValidationError(f'Слово "{word}" недопустимо в имени товара')
+        if name:
+            for word in ban_words:
+                if word in name.lower():
+                    raise ValidationError(f'Слово "{word}" недопустимо в имени товара')
         return name
 
     def clean_description(self):
         ban_words = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"]
         description = self.cleaned_data.get('description')
-        for word in ban_words:
-            if word in description.lower():
-                raise ValidationError(f'Слово "{word}" недопустимо в описании товара')
+        if description:
+            for word in ban_words:
+                if word in description.lower():
+                    raise ValidationError(f'Слово "{word}" недопустимо в описании товара')
         return description
 
 
     def clean_price(self):
         cleaned_data = super().clean()
         price = cleaned_data.get('price')
-        if price < 0:
+        if price and price < 0:
            raise ValidationError('Цена не может быть отрицательной')
         return price
 
@@ -47,8 +49,9 @@ class ProductForm(StyleFormMixin, ModelForm):
         image = cleaned_data.get('image')
         valid_extensions = ('jpg', 'png', 'jpeg')
         _, ext = image.name.rsplit('.', maxsplit=1)
-        if image.size > 5 * 1024 * 1024:
-            raise ValidationError("Размер файла не должен превышать 5 МБ.")
-        if ext not in valid_extensions:
-            raise ValidationError("Недопустимый формат файла. Загрузите JPEG или PNG.")
+        if image:
+            if image.size > 5 * 1024 * 1024:
+                raise ValidationError("Размер файла не должен превышать 5 МБ.")
+            if ext not in valid_extensions:
+                raise ValidationError("Недопустимый формат файла. Загрузите JPEG или PNG.")
         return image
