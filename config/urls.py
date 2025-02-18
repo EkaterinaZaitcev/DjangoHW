dc31729bb35b3342ad36dd9b1f40b3f9797810c0
EkaterinaZaitcev <katyat1@mail.ru>
2025-02-18 21:24:43 +1000

    Создано приложение users прописаны urls для регистрации

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,8 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
-    path('blog/', include('blog.urls', namespace='blog'))
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
